cec2df518a26d14a51c864d8bd4889bf640645a5
furplag <furplag@furplag.jp>
2017-01-31 18:58:50 +0900

    upgrade.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,50 +1,119 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
   'use strict';
 
   // Force use of Unix newlines
   grunt.util.linefeed = '\n';
 
-  var taskConfig = grunt.file.readJSON('./grunt/taskConfig.json', { encoding: 'utf8' });
+  RegExp.quote = function(string) {
+    return string.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&');
+  };
+
   var path = require('path');
 
   grunt.initConfig({
 
     // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
+    pkg : grunt.file.readJSON('package.json'),
+    banner : '/*!\n'
+      + ' * <%= pkg.name %> v<%= pkg.version %> (<%= pkg.homepage %>)\n'
+      + ' * Copyright 2014+ <%= pkg.author %>\n'
+      + ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n'
+      + ' * <%= pkg.description %>\n' + ' */\n',
 
-    clean: {
-      dist: 'build'
-    },
-
-    copy: {
-      build: {
-        src: ['src/firefoxManifest.webapp'],
-        dest: 'build/firefoxManifest.webapp'
-      }
-    },
-
-    favicons: {
-      options: taskConfig.favicons,
-      dist: {
+    realFavicon: {
+      favicons: {
         src: 'src/image.png',
-        dest: 'build/img'
+        dest: 'build/images',
+        options: {
+          iconsPath: '/',
+          html: [ 'dist/demo.html' ],
+          design: {
+            ios: {
+              pictureAspect: 'backgroundAndMargin',
+              backgroundColor: '#efefef',
+              margin: '4%',
+              assets: {
+                ios6AndPriorIcons: false,
+                ios7AndLaterIcons: true,
+                precomposedIcons: true,
+                declareOnlyDefaultIcon: true
+              }
+            },
+            desktopBrowser: {},
+            windows: {
+              pictureAspect: 'noChange',
+              backgroundColor: '#2d89ef',
+              onConflict: 'override',
+              assets: {
+                windows80Ie10Tile: true,
+                windows10Ie11EdgeTiles: {
+                  small: true,
+                  medium: true,
+                  big: true,
+                  rectangle: true
+                }
+              }
+            },
+            androidChrome: {
+              pictureAspect: 'backgroundAndMargin',
+              margin: '0%',
+              backgroundColor: '#efefef',
+              themeColor: '#efefef',
+              manifest: {
+                display: 'standalone',
+                orientation: 'notSet',
+                onConflict: 'override',
+                declared: true
+              },
+              assets: {
+                legacyIcon: false,
+                lowResolutionIcons: false
+              }
+            },
+            safariPinnedTab: {
+              pictureAspect: 'silhouette',
+              themeColor: '#5bbad5'
+            }
+          },
+          settings: {
+            compression: 2,
+            scalingAlgorithm: 'Lanczos',
+            errorOnImageTooSmall: false
+          }
+        }
       }
     },
 
-    tinypng: {
-      options: taskConfig.tinypng,
-      dist: {
-        src: '*.png',
-        cwd: 'build/img/',
-        dest: 'build/img/optimized/',
-        expand: true,
-        rename: function (dest, src) {
+    clean : {
+      dist : 'build',
+    },
+
+    tinypng : {
+      options : {
+        apiKey : "",
+        checkSigs : false,
+        debug : true,
+        showProgress : true,
+        sigFile : "",
+        stopOnImageError : true,
+        summarize : true
+      },
+      dist : {
+        src : '*.png',
+        cwd : 'build/images/',
+        dest : 'build/images/optimized/',
+        expand : true,
+        rename : function(dest, src) {
           return path.join(dest, src.split('/').pop())
         }
       }
     }
   });
 
-  require('load-grunt-tasks')(grunt, { scope: 'devDependencies' });
+  // These plugins provide necessary tasks.
+  require('load-grunt-tasks')(grunt, {scope : 'devDependencies'});
   require('time-grunt')(grunt);
-}
+
+  // Default task.
+  grunt.registerTask('default', [ 'realFavicon' ])
+};
--- README.md
+++ README.md
@@ -2,3 +2,9 @@ fav-icon-gen
 ============
 
 favicon resizer for me.
+
+@see [grunt-real-favicon](http://realfavicongenerator.net/).
+  
+  
+---
+2014+ furplag
